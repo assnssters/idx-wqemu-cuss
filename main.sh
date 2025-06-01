b89e1d4c1074a6a6eadad157bb90a7cc8981aab1
assnssters <dinhductri18@gmail.com>
2025-06-01 10:44:08 +0700

    Update main.sh

--- main.sh
+++ main.sh
@@ -12,6 +12,14 @@ yellow='\033[1;33m'
 blue='\033[1;34m'
 light_cyan='\033[1;96m'
 reset='\033[0m'
+# check session cũ
+if [ -e "session.env" ]; then
+  source session.env
+  
+fi
 
 # bắt đầu nhỉ nhiên là update package và tải mấy gói cần thiết á( do thêm cái ẩn nên ko thấy :))) )
-sudo apt update > /dev/null 2>&1
+print ""$yellow"Đang Update và Tải gói cần thiết..."
+sudo apt update -y > /dev/null 2>&1
+sudo apt install p7zip-full qemu-kvm -y > /dev/null 2>&1
+#
